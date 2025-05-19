88d4f69617537abc25a376efa279adcc6f704507
HuynhHoangLong <hoanglong27404@gmail.com>
2025-05-20 00:37:00 +0700

    feature/register

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const MainLayout = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ src/pages/CusHomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../components/layout/MainLayout'
+
+const CusHomePage = () => {
+  return (
+    <div>
+        <MainLayout/>
+
+    </div>
+  )
+}
+
+export default CusHomePage

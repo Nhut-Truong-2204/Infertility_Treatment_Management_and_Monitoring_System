0eca1f41babb3025082fb34def6b715e7efc0a88
HuynhHoangLong <hoanglong27404@gmail.com>
2025-06-06 02:30:51 +0700

    feat(Navbar): add scroll effect and improve layout structure

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import { useNavigate } from "react-router-dom";
@@ -10,10 +10,20 @@ import FemaleIcon from "@mui/icons-material/Female";
 import InfertilityIcon from "../../../assets/R.png";
 import { Syringe } from "phosphor-react";
 
-export default function Navbar() {  
+export default function Navbar() {
   const [openDropdown, setOpenDropdown] = useState(null);
   const navigate = useNavigate();
+  const [isScrolled, setIsScrolled] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      // Khi scroll xuống 50px thì bật trạng thái
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   const routes = {
     goHome: () => navigate("/"),
     goRegister: () => navigate("/register"),
@@ -25,194 +35,209 @@ export default function Navbar() {
     goDoctorList: () => navigate("/viewDoctorList"),
     goClinicIntro: () => navigate("/clinicpage"),
     goService: () => navigate("/servicepage"),
-
   };
 
   return (
-    <nav className="bg-[#032F6C] px-8 py-3 flex items-center justify-between text-white sticky top-0 z-50 shadow-md border-b border-blue-900">
-      {/* Logo */}
-      <div className="flex items-center space-x-2 font-semibold text-lg cursor-pointer">
-        <Stack direction="row" spacing={2}>
-          <Avatar sx={{ bgcolor: "#23A0FF" }}>
-            <Syringe size={20} weight="fill" color="white" />
-          </Avatar>
-        </Stack>
-        <span onClick={routes.goHome}>ReproTrack</span>
-      </div>
+    <div
+      className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
+        isScrolled ? "bg-[#183383]/90 shadow-lg" : "bg-transparent"
+      }`}
+    >
+      <nav className="  py-7 px-40 flex items-center justify-between text-white sticky top-0 z-50 shadow-md border-b-grey-1 border-grey-200">
+        {/* Logo */}
+        <div className="flex items-center space-x-2 font-semibold text-lg cursor-pointer">
+          <Stack direction="row" spacing={2}>
+            <img
+              src="https://demo.awaikenthemes.com/landing/wp-content/uploads/2025/04/ferlix-logo.svg"
+              alt=""
+            />
+          </Stack>
+          <span onClick={routes.goHome}>ReproTrack</span>
+        </div>
 
-      {/* Menu chính */}
-      <ul className="flex space-x-8">
-        {/* Học */}
-        <li
-          className="relative group"
-          onMouseEnter={() => setOpenDropdown(0)}
-          onMouseLeave={() => setOpenDropdown(null)}
-        >
-          <button
-            className="flex px-4 py-2 items-center gap-1 font-semibold uppercase 
+        {/* Menu chính */}
+        <ul className="flex space-x-8">
+          {/* Học */}
+          <li
+            className="relative group"
+            onMouseEnter={() => setOpenDropdown(0)}
+            onMouseLeave={() => setOpenDropdown(null)}
+          >
+            <button
+              className="flex px-4 py-2 items-center gap-1 font-semibold uppercase 
                        transition-all duration-300 
                        group-hover:text-[#032F6C] 
                        group-hover:bg-white 
                        group-hover:shadow 
                        rounded-md"
+            >
+              Tìm hiểu thêm
+              <ArrowDropDownIcon />
+            </button>
+
+            {openDropdown === 0 && (
+              <ul className="absolute -left-20 min-w-[350px] px-6 py-4 bg-white text-black z-20 rounded-lg shadow-xl border border-gray-200 transition-all duration-200">
+                <p className="text-[#132b6c] font-bold ml-3 mb-3">
+                  TÌM HIỂU THÊM
+                </p>
+                <div className="grid grid-cols-2 gap-4">
+                  <li
+                    onClick={routes.goBarrenMale}
+                    className="flex items-center gap-2 px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition"
+                  >
+                    Hiếm muộn Nam <MaleIcon fontSize="small" />
+                  </li>
+                  <li
+                    onClick={routes.goBarrenFeMale}
+                    className="flex items-center gap-2 px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition"
+                  >
+                    Hiếm muộn Nữ <FemaleIcon fontSize="small" />
+                  </li>
+                  <li
+                    onClick={routes.goInfertility}
+                    className="flex items-center gap-2 px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition relative"
+                  >
+                    Vô sinh
+                    <img
+                      src={InfertilityIcon}
+                      alt="infertility"
+                      className="w-[26px] absolute right-4 top-1/2 -translate-y-1/2"
+                    />
+                  </li>
+                  <li
+                    onClick={routes.goClinicIntro}
+                    className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition"
+                  >
+                    Clinic Information
+                  </li>
+                  <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
+                    Link 3
+                  </li>
+                </div>
+              </ul>
+            )}
+          </li>
+
+          {/* Chăm sóc */}
+          <li
+            className="relative group"
+            onMouseEnter={() => setOpenDropdown(1)}
+            onMouseLeave={() => setOpenDropdown(null)}
           >
-            Tìm hiểu thêm
-            <ArrowDropDownIcon />
-          </button>
-
-          {openDropdown === 0 && (
-            <ul className="absolute -left-20 min-w-[350px] px-6 py-4 bg-white text-black z-20 rounded-lg shadow-xl border border-gray-200 transition-all duration-200">
-              <p className="text-[#132b6c] font-bold ml-3 mb-3">TÌM HIỂU THÊM</p>
-              <div className="grid grid-cols-2 gap-4">
-                <li
-                  onClick={routes.goBarrenMale}
-                  className="flex items-center gap-2 px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition"
-                >
-                  Hiếm muộn Nam <MaleIcon fontSize="small" />
-                </li>
-                <li
-                  onClick={routes.goBarrenFeMale}
-                  className="flex items-center gap-2 px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition"
-                >
-                  Hiếm muộn Nữ <FemaleIcon fontSize="small" />
-                </li>
-                <li
-                  onClick={routes.goInfertility}
-                  className="flex items-center gap-2 px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition relative"
-                >
-                  Vô sinh
-                  <img
-                    src={InfertilityIcon}
-                    alt="infertility"
-                    className="w-[26px] absolute right-4 top-1/2 -translate-y-1/2"
-                  />
-                </li>
-                <li onClick={routes.goClinicIntro} className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
-                  Clinic Information
-                </li>
-                <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
-                  Link 3
-                </li>
-              </div>
-            </ul>
-          )}
-        </li>
-
-        {/* Chăm sóc */}
-        <li
-          className="relative group"
-          onMouseEnter={() => setOpenDropdown(1)}
-          onMouseLeave={() => setOpenDropdown(null)}
-        >
-          <button
-            className="flex px-4 py-2 items-center gap-1 font-semibold uppercase 
+            <button
+              className="flex px-4 py-2 items-center gap-1 font-semibold uppercase 
                        transition-all duration-300 
                        group-hover:text-[#032F6C] 
                        group-hover:bg-white 
                        group-hover:shadow 
                        rounded-md"
+            >
+              Chăm sóc
+              <ArrowDropDownIcon />
+            </button>
+
+            {openDropdown === 1 && (
+              <ul className="absolute -left-20 min-w-[350px] px-6 py-4 bg-white text-black z-20 rounded-lg shadow-xl border border-gray-200 transition-all duration-200">
+                <div className="grid grid-cols-2 gap-4">
+                  <li
+                    onClick={routes.goTestingList}
+                    className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition"
+                  >
+                    Dịch vụ của chúng tôi
+                  </li>
+                  <li
+                    onClick={routes.goDoctorList}
+                    className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition"
+                  >
+                    Danh sách bác sĩ
+                  </li>
+                  <li
+                    onClick={routes.goService}
+                    className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition"
+                  >
+                    Dịch vụ bệnh viện
+                  </li>
+                </div>
+              </ul>
+            )}
+          </li>
+
+          {/* Tham gia */}
+          <li
+            className="relative group"
+            onMouseEnter={() => setOpenDropdown(2)}
+            onMouseLeave={() => setOpenDropdown(null)}
           >
-            Chăm sóc
-            <ArrowDropDownIcon />
-          </button>
-
-          {openDropdown === 1 && (
-            <ul className="absolute -left-20 min-w-[350px] px-6 py-4 bg-white text-black z-20 rounded-lg shadow-xl border border-gray-200 transition-all duration-200">
-              <div className="grid grid-cols-2 gap-4">
-                <li
-                  onClick={routes.goTestingList}
-                  className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition"
-                >
-                  Dịch vụ của chúng tôi
-                </li>
-                <li onClick={routes.goDoctorList}
-                className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
-                  Danh sách bác sĩ 
-                </li>
-                <li onClick={routes.goService}
-                className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
-                  Dịch vụ bệnh viện
-                </li>
-              </div>
-            </ul>
-          )}
-        </li>
-
-        {/* Tham gia */}
-        <li
-          className="relative group"
-          onMouseEnter={() => setOpenDropdown(2)}
-          onMouseLeave={() => setOpenDropdown(null)}
-        >
-          <button
-            className="flex px-4 py-2 items-center gap-1 font-semibold uppercase 
+            <button
+              className="flex px-4 py-2 items-center gap-1 font-semibold uppercase 
                        transition-all duration-300 
                        group-hover:text-[#032F6C] 
                        group-hover:bg-white 
                        group-hover:shadow 
                        rounded-md"
-          >
-            Tham gia
-            <ArrowDropDownIcon />
-          </button>
+            >
+              Tham gia
+              <ArrowDropDownIcon />
+            </button>
 
-          {openDropdown === 2 && (
-            <ul className="absolute -left-20 min-w-[350px] px-6 py-4 bg-white text-black z-20 rounded-lg shadow-xl border border-gray-200 transition-all duration-200">
-              <div className="grid grid-cols-2 gap-4">
-                <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
-                  Link 1
-                </li>
-                <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
-                  Link 2
-                </li>
-                <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
-                  Link 3
-                </li>
-                <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
-                  Link 4
-                </li>
-              </div>
-            </ul>
-          )}
-        </li>
-      </ul>
+            {openDropdown === 2 && (
+              <ul className="absolute -left-20 min-w-[350px] px-6 py-4 bg-white text-black z-20 rounded-lg shadow-xl border border-gray-200 transition-all duration-200">
+                <div className="grid grid-cols-2 gap-4">
+                  <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
+                    Link 1
+                  </li>
+                  <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
+                    Link 2
+                  </li>
+                  <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
+                    Link 3
+                  </li>
+                  <li className="px-4 py-2 hover:bg-blue-100 cursor-pointer rounded transition">
+                    Link 4
+                  </li>
+                </div>
+              </ul>
+            )}
+          </li>
+        </ul>
 
-      {/* Nút Đăng nhập & Đăng ký */}
-      <div className="flex space-x-4">
-        <Button
-          onClick={routes.goLogin}
-          variant="outlined"
-          sx={{
-            color: "white",
-            borderColor: "white",
-            borderRadius: "8px",
-            textTransform: "none",
-            px: 3,
-            "&:hover": {
-              backgroundColor: "white",
-              color: "#032F6C",
+        {/* Nút Đăng nhập & Đăng ký */}
+        <div className="flex space-x-4">
+          <Button
+            onClick={routes.goLogin}
+            variant="outlined"
+            sx={{
+              color: "white",
               borderColor: "white",
-            },
-          }}
-        >
-          Đăng nhập
-        </Button>
-        <Button
-          onClick={routes.goRegister}
-          variant="contained"
-          sx={{
-            backgroundColor: "#23A0FF",
-            borderRadius: "8px",
-            textTransform: "none",
-            px: 3,
-            "&:hover": {
-              backgroundColor: "#1B7ACD",
-            },
-          }}
-        >
-          Đăng ký
-        </Button>
-      </div>
-    </nav>
+              borderRadius: "8px",
+              textTransform: "none",
+              px: 3,
+              "&:hover": {
+                backgroundColor: "white",
+                color: "#032F6C",
+                borderColor: "white",
+              },
+            }}
+          >
+            Đăng nhập
+          </Button>
+          <Button
+            onClick={routes.goRegister}
+            variant="contained"
+            sx={{
+              backgroundColor: "#23A0FF",
+              borderRadius: "8px",
+              textTransform: "none",
+              px: 3,
+              "&:hover": {
+                backgroundColor: "#1B7ACD",
+              },
+            }}
+          >
+            Đăng ký
+          </Button>
+        </div>
+      </nav>
+    </div>
   );
 }
--- src/pages/customer/HomePage.jsx
+++ src/pages/customer/HomePage.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import MainLayout from '../../components/layout/MainLayout'
-import BookAppointmentSection from '../../components/DashboardComponents/BookAppointCom'
-import Welcome from '../../components/DashboardComponents/WelcomeCom'
+import React from "react";
+import MainLayout from "../../components/layout/MainLayout";
+import BookAppointmentSection from "../../components/DashboardComponents/BookAppointCom";
+import Welcome from "../../components/DashboardComponents/WelcomeCom";
+import Header from "../../components/layout/Header";
 const HomePage = () => {
   return (
-    <div className='flex flex-col items-center  min-h-screen bg-gray-100'>
-      <div className='bg-amber-500 w-screen  flex items-center justify-center '>
+    <>
+    <Header/>
+    <div className=' w-screen  flex items-center justify-center '>
         <Welcome/>
       </div>
-      <div className=' w-screen  flex items-center justify-center '>
+    <div className=' w-screen flex items-center justify-center '>
         <BookAppointmentSection/>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;

b9f0c87caf8a43f6676407c1158e650a3022e648
hoanglong <haonglong27404@gmail.com>
2025-06-10 16:56:25 +0700

    feat(DoctorList).jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import LoadingPage from './components/layout/LoadingPage';
+import   router  from "../src/router/router";
+import { RouterProvider } from "react-router";
 
-export default function App() {
-  return <LoadingPage />;
+function App() {
+  return <RouterProvider router={router} />;
 }
+
+export default App;

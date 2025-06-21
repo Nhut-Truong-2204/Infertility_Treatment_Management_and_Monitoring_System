ecce4dec952e22edcc1922dc66de6ff8e2cb009f
TranPhuocAn <antpse180115@fpt.edu.vn>
2025-06-21 15:10:43 +0700

    fix(logo): fix logo pages

--- src/pages/customer/ForgetPasswordPage.jsx
+++ src/pages/customer/ForgetPasswordPage.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { Typography, Alert, Avatar, Stack } from "@mui/material";
+import { Typography, Alert } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import { Syringe } from "phosphor-react";
 import Background from "../../assets/ForgotPassword.jpg";
 import { requestPasswordReset, validateResetToken } from "../../api/customer/forgotPassword";
+import logo from "../../../public/LogoWithoutText.png";
 
 export default function ForgetPasswordPage() {
     const navigate = useNavigate();
@@ -209,11 +209,7 @@ export default function ForgetPasswordPage() {
                                 transition: { duration: 0.2 }
                             }}
                         >
-                            <Stack direction="row" spacing={2}>
-                                <Avatar sx={{ bgcolor: "#23A0FF" }}>
-                                    <Syringe size={20} weight="fill" color="white" />
-                                </Avatar>
-                            </Stack>
+                            <img src={logo} alt="CumIcon" className="h-12" />
                             <motion.span
                                 onClick={() => navigate('/')}
                                 whileTap={{ scale: 0.95 }}
